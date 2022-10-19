fdeaa2530c452bbf703598e9e4e25bdbc1de21f5
jorgepmtz <jorge.pmtz@gmail.com>
2022-10-19 07:14:16 -0500

    20221018-StartJQuery2

---
+++ js/01-selectores.js
@@ -0,0 +1,10 @@
+'use strict';
+// ------ puedo usar $ o puedo usar JQuery cualquier de los dos se refieren al objeto JQuery
+// ------ entre parentesis (document) va el selector y despues .ready calquier método
+$(document).ready(function(){
+  console.log('Estamos listos con JQuery !!')
+  
+
+})
+
+ 
