0d87d9c1dd3608c039aa42da2208c51aa0fdff05
jorgepmtz <jorge.pmtz@gmail.com>
2022-11-23 20:07:16 -0600

    20221123-AjaxJQuery

--- js/05-ajax.js
+++ js/05-ajax.js
@@ -11,9 +11,45 @@ $(document).ready(function(){
     response.data.forEach((element, index) => {
       $('#id_datos').append('<p>' +element.first_name+' '+element.last_name+'</p>')
     });
-
-
   })  
+  
+  $('#id_formulario').submit(function(e){
+    e.preventDefault();
+    var usuario = {
+      name : $('input[name="name"]').val(),
+      web : $('input[name="web"]').val()
+    };
+    console.log('usuario', usuario);
+    /*
+    $.post($(this).attr('action'), usuario, function(response){
+      console.log('post reponse', response);
+    }).done(function(){
+      alert('usuario añadido correctamente');
+    });
+    */
+    // ------------------- Usando $.ajax -----------------------
+
+    $.ajax({
+      type :  'POST',
+      dataType :  'json',               // ------------- Esto ni es necesario es opcional ------------
+      cotentType : 'application/json',  // ------------- Esto ni es necesario es opcional ------------
+      url : $(this).attr('action'),
+      data : usuario,
+      beforeSend : function(){
+        console.log('Enviando registro . . . ')
+      },
+      success : function(response){
+        console.log('seccess(response)', response);
+      },
+      error : function(){
+        console.log('error', 'Ha ocurrido un error . . . . ');        
+      },
+      timeout : 1000
+    });
+
+
+    return false;
+  });  //------------------- +++++++++++++ Fin de .submit +++++++++++++++++-------------
 
 
 }); //------------------------ Fin de $(document).ready() -------------------
